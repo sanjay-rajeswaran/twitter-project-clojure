6889064e52aa73f6e34895bf455ff9d0e4144220
Sanjay Rajeswaran <sanjay_r@Sanjays-MacBook-Air.local>
2018-09-05 17:05:43 +0530

    update

--- src/clj/twitter_port/twitter_functions.clj
+++ src/clj/twitter_port/twitter_functions.clj
@@ -1,9 +1,14 @@
 (ns twitter-port.twitter-functions
-  (:use [twitter.oauth]
-        [twitter.callbacks]
-        [twitter.callbacks.handlers]
-        [twitter.api.restful])
+  (:require
+  	[twitter.oauth :refer [make-oauth-creds]]
+  	[twitter.api.restful :as twt])
   (:import [twitter.callbacks.protocols SyncSingleCallback]))
+
+  ; (:use [twitter.oauth]
+  ;       [twitter.callbacks]
+  ;       [twitter.callbacks.handlers]
+  ;       [twitter.api.restful])
+  ; (:import [twitter.callbacks.protocols SyncSingleCallback]))
 
 (def my-creds (make-oauth-creds "YfJKQCqZ7CkLVD9IBuRI4aErn"
                                 "4yvAZ9rskRLTlnmV2Yb61xzpeiGBwd6ibpOX0GKlmxZHTj41Nz"
@@ -11,4 +16,4 @@
                                 "3qAax2aRlYI7V8HbrM2tdhFyOQNwbEdf5rw3NXJUKPlGi"))
 
 (defn retrieve-tweets [user-id]
-  (statuses-user-timeline :oauth-creds my-creds :params {:screen-name user-id}))
+  (twt/statuses-user-timeline :oauth-creds my-creds :params {:screen-name user-id}))
